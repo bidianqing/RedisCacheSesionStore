acae22f85e60791ce394d45b2fc507a9afce1d75
毕殿卿 <bdq123@outlook.com>
2017-12-21 15:36:39 +0800

    添加项目文件。

---
+++ RedisCacheSesionStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedisCacheSesionStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
